7845d4f699173c537d1a6f0a9da95da5d1010e95
Deathby-Snusnu <hw8041@aol.com>
2025-03-23 18:00:48 +0100

    Aktualisiere die Benennung von Konstanten in der Konfigurationsdatei für bessere Klarheit und Konsistenz.

--- src/config/const.py
+++ src/config/const.py
@@ -1,9 +1,9 @@
-DPI: int = 300
-PERLEN_GROESSE: float = 2.5
-FARBBEREICH: int = 64
-FORMAT: str = "A4"
-MM_PRO_INCH: float = 25.4
+PRINTRESOLUTIONDPI: int = 300
+PEARL_SIZE: float = 2.5
+COLOR_DEPTH: int = 64
+PAGE_FORMAT: str = "A4"
+MILLIMETERS_PER_INCH: float = 25.4
 
 class GUI:
-    GEOMETRY: str = '600x400'
-    TITLE: str = "Diamond Perls Generator"
+    WINDOW_DIMENSIONS: str = '600x400'
+    WINDOW_TITLE: str = "Diamond Perls Generator"
